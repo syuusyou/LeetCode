57543ccff6a00d8e1bdc164d5919fdb67b4386fc
Chen, Hsiu-Chang <syuusyou@gmail.com>
2017-03-22 19:27:01 +0800

    add: 104 solution

---
+++ 104_Maximum_Depth_of_Binary_Tree_1.c
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) 
+{
+    int left, right;
+    
+    if(root)
+    {
+        left = maxDepth(root->left);
+        right = maxDepth(root->right);
+        
+        if(left > right)
+        {
+            return left + 1;
+        }
+        else
+        {
+            return right + 1;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
