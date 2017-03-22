138e21cf9e3b066600712dd0263b6d637e4243c2
Chen, Hsiu-Chang <syuusyou@gmail.com>
2017-03-22 19:29:04 +0800

    add: 535 solution

---
+++ 535_Encode_and_Decode_TinyURL_1.c
@@ -0,0 +1,15 @@
+/* I am too lazy LoL */
+
+/** Encodes a URL to a shortened URL. */
+char* encode(char* longUrl) {
+    return longUrl;
+}
+
+/** Decodes a shortened URL to its original URL. */
+char* decode(char* shortUrl) {
+    return shortUrl;
+}
+
+// Your functions will be called as such:
+// char* s = encode(s);
+// decode(s);
