a06a812a30251b689eb69086efcbea22c45bb38c
Chen, Hsiu-Chang <syuusyou@gmail.com>
2017-03-22 19:27:12 +0800

    add: 136 solution

---
+++ 136_Single_Number_1.c
@@ -0,0 +1,12 @@
+int singleNumber(int* nums, int numsSize) 
+{
+    int result = 0;
+    int i;
+    
+    for(i = 0; i < numsSize; i++)
+    {
+        result ^= nums[i];
+    }
+    
+    return result;
+}
